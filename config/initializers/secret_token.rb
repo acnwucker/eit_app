a050d4b2d0d1b9cf93c39bda969ae31908bae28f
Jordan Betteridge <jordandbetteridge@gmail.com>
2012-05-16 13:28:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EitApp::Application.config.secret_token = '4b9b1eab32bc5c6e3c6649aa2147d060017de63d0c4415b30ee7efafafa6b9ac97627c4b2fb3406fa4d3a71d75d7a180af0924b5c27d9f791859a395b238bb04'
